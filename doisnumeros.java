40900a46c0e97c8a1cf1b454b646fb6c2cf93da4
victrferreira <silvabh88@live.com>
2025-03-03 16:17:51 -0300

    Add files via upload

---
+++ doisnumeros.java
@@ -0,0 +1,16 @@
+package doisnumeros;
+
+public class doisnumeros {
+    public static void main(String[] args) {
+        int number1 = 4;
+        int number2 = 2;
+        int subtração = number1 - number2;
+        int multiplicação = number1 * number2;
+        int divisão = number1 / number2;
+        System.out.println("number1 = " + number1);
+        System.out.println("number2 = " + number2);
+        System.out.println("Valor de number1 - number2 = " + subtração);
+        System.out.println("number1 x number2= " + multiplicação);
+        System.out.println("number1 dividido por number2= " + divisão);
+    }
+}
