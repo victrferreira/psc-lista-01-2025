a1273ced7c9a16ced7a0b4ef9541461f49413512
victrferreira <silvabh88@live.com>
2025-03-03 16:23:28 -0300

    Add files via upload

---
+++ somadoisnumeros.java
@@ -0,0 +1,10 @@
+public class somadoisnumeros {
+    public static void main(String[] args) {
+        int number1 = 4;
+        int number2 = 2;
+        int soma = number1 + number2;
+        System.out.println("number1 = " + number1);
+        System.out.println("number2 = " + number2);
+        System.out.println("Valor de number1 + number2 = " + soma);
+    }
+}
